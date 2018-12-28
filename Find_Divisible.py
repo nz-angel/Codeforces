aa5a54b24e23b537af19c7b33aacf02790e196b3
Naza <nafm@live.com.ar>
2018-12-28 11:58:56 -0300

    Aceptado

---
+++ Find_Divisible.py
@@ -0,0 +1,18 @@
+n_queries = int(input())
+res = []
+
+for k in range(n_queries):
+    l, r = list(map(int, input().split()))
+    if l == 1:
+        res.append((1, r))
+    else:
+        flag = True
+        while flag:
+            if 2 * l <= r:
+                res.append((l, 2*l))
+                flag = False
+            else:
+                l += 1
+
+for r in res:
+    print('{} {}'.format(r[0], r[1]))
